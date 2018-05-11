9a02e421a8a52671c35af050bf407ba99bbdfc24
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-11 17:22:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if number % 3 == 0
      "Fizz"
-   if number % 5 == 0
+   elsif number % 5 == 0
      "Buzz"
-   if number % 3 && number % 5 == 0
+   elsif number % 3 && number % 5 == 0
      "FizzBuzz"
      
   end
